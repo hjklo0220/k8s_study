0124a1a93c4ee0f0ab9b6778b9ae84da0972c98d
JunYoung_Kim <hjklo0220@gmail.com>
2023-09-05 16:16:26 +0900

    230905 k8s study

--- fortune/args/fortuneloop.sh
+++ fortune/args/fortuneloop.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 mkdir -p /var/www
+
+INTERVAL=$1
+echo "Generate new fortune every $INTERVAL seconds"
+
 while true
 do
     echo $(date) Writing fortune ot /var/www/index.html
     /usr/games/fortune > /var/www/index.html
-    sleep 10
+    sleep $INTERVAL
 done
-
--- fortune/env/fortuneloop.sh
+++ fortune/env/fortuneloop.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 mkdir -p /var/www
+
+echo "Generate new fortune every $INTERVAL seconds"
+
 while true
 do
     echo $(date) Writing fortune ot /var/www/index.html
     /usr/games/fortune > /var/www/index.html
-    sleep 10
+    sleep $INTERVAL
 done
-
